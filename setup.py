1c03c15d6c43c032afa90dc24c2dc4baed2ba723
Jesper Dramsch <jesper@dramsch.net>
2019-02-07 07:20:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,28 @@ with open('README.md') as f:
 
 
 setup(
-    name='DeepComplexNetworks',
-    version='1',
+    name='keras-complex',
+    version='0.1.1',
     license='MIT',
     long_description=DESCRIPTION,
+    url='https://github.com/JesperDramsch/keras-complex',
     packages=find_packages(),
     scripts=['scripts/run.py', 'scripts/training.py'],
     install_requires=[
-        "numpy", "scipy", "sklearn", "tensorflow", "keras"]
+        "numpy", "scipy", "sklearn", "keras"]
+    extras_require={
+        "tf": ["tensorflow"],
+        "tf_gpu": ["tensorflow-gpu"],
+    }
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3 :: Only',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
 )
